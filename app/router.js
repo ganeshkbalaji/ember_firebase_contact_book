b3ff7d067469550841d2a41bec66fe4c893e4445
Ganesh Balaji <ganeshkbalaji@ymail.com>
2016-03-19 20:53:20 -0700

    Updates router.js

--- app/router.js
+++ app/router.js
@@ -1,16 +1,21 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+
   this.route('about');
   this.route('contact');
 
   this.route('admin', function() {
     this.route('invitations');
+  });
+
+  this.route('libraries', function() {
+    this.route('new');
   });
 });
 
